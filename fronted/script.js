46f0619723fdb61c36d901fd6a5a3a77ab9c1fa1
kamalakanta Giri <kamalakantagiri884@gmail.com>
2026-09-19 23:28:14 +0530

    Fix Render API URL and 0-10 score

--- fronted/script.js
+++ fronted/script.js
@@ -210,7 +210,7 @@ form.addEventListener(
 
             const response =
                 await fetch(
-                    " https://mental-health-score-db4n.onrender.com",
+                    "https://mental-health-score-db4n.onrender.com/predict",
                     {
 
                         method:
